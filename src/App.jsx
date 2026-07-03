646f34f30e032c6bd50b411f5bca0801cb93f232
Pragdeesh kumar <padma.pons@gmail.com>
2026-07-03 21:21:37 +0530

    nav-complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { ScrollTrigger, SplitText } from "gsap/all";
+import gsap from "gsap";
+import Navbar from "./components/Navbar.jsx";
 
 gsap.registerPlugin( ScrollTrigger, SplitText );
 
 const App = () => {
     return (
-        <div>
+        <main>
+            <Navbar/>
 
-        </div>
+        </main>
     )
 }
 export default App
